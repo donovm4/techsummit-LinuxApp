deaf43a09c9e4c3550bc74cd740230e9ec0cd448
Donovan McCoy <144926888+donovm4@users.noreply.github.com>
2025-09-25 17:23:13 -0400

    query table from sql server

--- LinuxApp/views.py
+++ LinuxApp/views.py
@@ -27,30 +27,56 @@ def contact():
         message='Your contact page.'
     )
 
-def query_db():
+# def query_db_customers():
+#     # Could potentially move some of the database connection outside to enhance overall speed of query function
+#     connect_str = "DRIVER={ODBC Driver 18 for SQL Server};SERVER=9.169.70.172,1433;DATABASE=MyAppDB;UID=SQLAdmin;PWD=P@55w0rd1234;Encrypt=no;TrustServerCertificate=yes;"
+#     with pyodbc.connect(connect_str) as conn:
+#         with conn.cursor() as cursor:
+#             cursor.execute(
+#                 "SELECT CustomerID, FirstName, LastName, Email FROM Customers"
+#             )
+#             # map column names to values
+#             # cols = [col[0] for col in cursor.description]
+#             # return [dict(zip(cols, row)) for row in cursor.fetchall()]
+
+#             # simplified
+#             customers = cursor.fetchall()
+#             return customers
+
+# @app.route('/customers')
+# def customers():
+#     customers = query_db_people()
+#     return render_template('customers.html',
+#                            title='MyAppDB',
+#                            year=datetime.now().year,
+#                            customers=customers,
+#                            message='Let\'s query the Customers table of our database!'
+#                            )
+
+def query_db_people():
     # Could potentially move some of the database connection outside to enhance overall speed of query function
-    connect_str = "DRIVER={ODBC Driver 18 for SQL Server};SERVER=9.169.70.172,1433;DATABASE=MyAppDB;UID=SQLAdmin;PWD=P@55w0rd1234;Encrypt=no;TrustServerCertificate=yes;"
+    connect_str = "DRIVER={ODBC Driver 18 for SQL Server};SERVER=0.0.0.0,1433;DATABASE=AdventureWorksPTO;UID=appuser;PWD=???;Encrypt=no;TrustServerCertificate=yes;"
     with pyodbc.connect(connect_str) as conn:
         with conn.cursor() as cursor:
             cursor.execute(
-                "SELECT CustomerID, FirstName, LastName, Email FROM Customers"
+                "SELECT Title, FirstName, MiddleName, LastName FROM Person.Person"
             )
             # map column names to values
             # cols = [col[0] for col in cursor.description]
             # return [dict(zip(cols, row)) for row in cursor.fetchall()]
 
             # simplified
-            customers = cursor.fetchall()
-            return customers
+            people = cursor.fetchall()
+            return people
 
-@app.route('/customers')
-def customers():
-    customers = query_db()
+@app.route('/people')
+def people():
+    people = query_db_people()
     return render_template('customers.html',
-                           title='MyAppDB',
+                           title='AdventureWorksPTO',
                            year=datetime.now().year,
-                           customers=customers,
-                           message='Let\'s query the Customers table of our database!'
+                           people=people,
+                           message='Let\'s query the Person.Person table of our database!'
                            )
 
 @app.route('/about')
@@ -62,3 +88,4 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
